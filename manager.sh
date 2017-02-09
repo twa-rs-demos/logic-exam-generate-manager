75bbe5044991a6cb14b3457d7b8d5a5c47274753
Afaren <550047450@qq.com>
2017-02-09 11:05:31 +0800

    afaren - finish skeleton

--- manager.sh
+++ manager.sh
@@ -1,36 +1,33 @@
 #! /usr/bin/env bash
 
-set -x
-
-#if node process alive 
-#	do nothing
-#	exit successfully
-#else then
-#	1. verify after
-#	2. move files
-#	3. restart
+#set -x
 
 
 
 is_node_process_alive() {
-	return 0
-	echo 'determine if node process is alive?'
+	[[ 2 = $( ps -e j | grep 'node spec/gen_output.js' | wc -l) ]]  && return 0
+	echo 'determine if node process is alive?' || return 1
+	
 }
 
-do_nothing_and_exit_if_node_process_alive() {
-	is_node_process_alive && exit 0 
-	exit 1
-}
 
 process_files() {
 	echo 'process files'
 }
 
 restart_node_process() {
-	echo 'process files'
+	echo 'restart node process'
 }
 
 
-do_nothing_and_exit_if_node_process_alive || ( process_files && restart_node_process )
 
-	
+
+while true; do 
+	if  ! is_node_process_alive ; then 
+		process_files
+		restart_node_process
+	fi
+	echo do_nothing # to be deleted
+	sleep 5s
+done
+
