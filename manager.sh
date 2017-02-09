742aa44bec7f9cc946ae311555b3b307e7da10c4
Afaren <550047450@qq.com>
2017-02-09 14:26:05 +0800

    afaren - cleanup

--- manager.sh
+++ manager.sh
@@ -1,6 +1,5 @@
 #! /usr/bin/env bash
 
-#set -x
 
 
 is_node_process_alive() {
@@ -10,20 +9,19 @@ is_node_process_alive() {
 
 
 handle_generated_files() {
-	echo 'handleing generated files'
 
-	echo 'make a new directory to save json and png' 
+	# make a new directory to save json and png 
 	directory="$(date +%Y_%m_%d_%H_%M_%S)"
 	mkdir $directory || exit 1 # 新建文件夹失败的异常
 
-	echo 'do second step & move files into directory'
+	# do second step & move files into directory
 	node spec/gen_workable_exam_graph_after_verify.js
 	mv workable_exam_after_verify/* $directory
 	rm workable_exam_before_verify/* 
 }
 
 restart_node_process() {
-	echo 'restart node process'
+	# restart node process in background
 	nohup node ./spec/gen_output.js > /dev/null &
 }
 
