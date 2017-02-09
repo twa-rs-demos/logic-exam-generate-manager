95352c27c13ba193b709146b036181b193ec148b
Afaren <550047450@qq.com>
2017-02-09 14:33:14 +0800

    afaren - add description

--- manager.sh
+++ manager.sh
@@ -1,5 +1,8 @@
 #! /usr/bin/env bash
 
+# 监测 logic-exam-generate node 进程是否运行中
+# 1. 运行中，不做任何操作，休眠十分钟后重新开始监测
+# 2. 已停止，将生成的文件挪到文件夹中保存，并重启 node 进程
 
 
 is_node_process_alive() {
@@ -34,6 +37,6 @@ while true; do
 		restart_node_process
 	fi
 	echo sleep  # to be deleted
-	sleep 2s
+	sleep 10m    # 控制休眠时间
 done
 
