f0cce90fe2ce6242af5087c0502e6f0c29a76c89
Afaren <550047450@qq.com>
2017-02-09 13:18:54 +0800

    afaren - finish restart & fix grep error

--- manager.sh
+++ manager.sh
@@ -1,22 +1,29 @@
 #! /usr/bin/env bash
 
-set -x
+#set -x
 
 
 
 is_node_process_alive() {
-	[[ 2 = $( ps -e j | grep 'node spec/gen_output.js' | wc -l) ]]  && return 0
+	[[ 2 = $( ps -e j | grep 'node ./spec/gen_output.js' | wc -l) ]]  && return 0
 	return 1 # dead
 	
 }
 
 
 process_files() {
-	echo 'process files'
+	echo 'processing files'
+	echo 'make a new directory to save json and png' 
+	echo 'do second step & move files into directory'
+#	directory = make_directory
+#	helper directory # helper 是以前在 服务器 上写的脚本，一会儿把它抄过来
+	
+
 }
 
 restart_node_process() {
 	echo 'restart node process'
+	nohup node ./spec/gen_output.js > /dev/null &
 }
 
 
@@ -28,6 +35,6 @@ while true; do
 		restart_node_process
 	fi
 	echo sleep  # to be deleted
-	sleep 5s
+	sleep 2s
 done
 
